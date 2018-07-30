bee39f9ebf75f13cb2b254c4c2b861e24a75a2c7
Kiarash Kiani <hi@kiarash.info>
2018-07-30 21:13:23 +0430

    fixed hardlim func and error calculation

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from perceptron import *
 import numpy
 
+
 def main():
-	p = perceptron()
-	p.baias = 0
-	for i in range(50):
-		p.train(numpy.array([1, 0]), 0)
-		p.train(numpy.array([0, 1]), 1)
-		p.train(numpy.array([1, 1]), 0)
-		p.train(numpy.array([0, 0]), 0)
-	
-	print(p.calculate([0, 0]))
-	print(p.calculate([1, 0]))
-	print(p.calculate([0, 1]))
-	print(p.calculate([1, 1]))
-	
-	print(p.weight)
-	
+    p = perceptron()
+    p.baias = 1
+    for i in range(50):
+        p.train(numpy.array([0, 0]), 1)
+        p.train(numpy.array([1, 0]), 0)
+        p.train(numpy.array([0, 1]), 0)
+        p.train(numpy.array([1, 1]), 0)
+
+    print(p.calculate([0, 0]))
+    print(p.calculate([1, 0]))
+    print(p.calculate([0, 1]))
+    print(p.calculate([1, 1]))
+
+    print("weights -> ", p.weight)
+    print("baias -> ", p.baias)
+
 if __name__ == "__main__":
-	main()
+    main()
--- perceptron.py
+++ perceptron.py
@@ -1,27 +1,25 @@
 import numpy
 
+
 class perceptron():
-	
-	def __init__(self):
-		self.weight = numpy.array([0, 0])
-		self.baias = 0
-		
-	def train(self, inVector, out):
-		calculated = numpy.dot(self.weight, inVector) + self.baias
-		out_c = self.stepUnit(calculated)
-		if out_c > out:
-			self.weight = numpy.subtract(self.weight, inVector)
-		elif out_c < out:
-			self.weight = numpy.add(self.weight, inVector)
-			
-	def calculate(self, inVector):
-		out = numpy.dot(self.weight, inVector)
-		return self.stepUnit(out)
-		
-	def stepUnit(self, inVector):
-		if inVector > 0:
-			return 1
-		elif inVector <= 0:
-			return 0
-		
-		
+    def __init__(self):
+        self.weight = numpy.array([0, 0])
+        self.baias = 0
+
+    def train(self, inVector, out):
+        out_c = self.calculate(inVector)
+        error = out - out_c
+
+        self.weight = numpy.add(self.weight, numpy.dot(error, inVector))
+        self.baias = self.baias + error
+
+    def calculate(self, inVector):
+        out = numpy.dot(self.weight, inVector) + self.baias
+        return self.hardlim(out)
+
+
+    def hardlim(self, inVector):
+        if inVector > 0:
+            return 1
+        elif inVector <= 0:
+            return 0
